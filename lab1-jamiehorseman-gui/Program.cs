ef330201a8fb9fdf812f2ff814d69494ebad56b3
jhorse416 <jamiehorseman416@gmail.com>
2020-02-03 21:25:42 -0500

    insert another meaningful message here

---
+++ lab1-jamiehorseman-gui/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lab1_jamiehorseman_gui
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
